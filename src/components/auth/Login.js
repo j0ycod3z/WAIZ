379d12abe382a3eb7afadce4e2587765255a9e1a
batchmarave <batchmarave@gmail.com>
2024-09-12 01:19:48 +0800

    remove s3 link

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -95,7 +95,7 @@ class Login extends React.Component {
                 <div className={cx("row", "d-flex", "justify-content-center")}>
                   <div className={cx("d-flex", "align-items-end")}>
                     <p className={cx(c.footer)}>
-                      wAIz © </a>
+                      wAIz © 
                     </p>
                   </div>
                 </div>
