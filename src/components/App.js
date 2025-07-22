4cea3a55fa672eb483f364edfd9ac70d4eab73ec
MaerRand <MakotoAine@proton.me>
2025-07-22 17:11:11 +0800

    Slight change
    Slowly working to upgrade react router v6 will continue tom

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
-import * as React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom'
-=======
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
->>>>>>> Stashed changes
 
 import Home from 'components/Home';
 import Login from 'components/auth/Login';
@@ -13,30 +8,9 @@ import Signup from 'components/auth/Signup';
 import SignupWelcome from 'components/auth/SignupWelcome';
 import Recovery from 'components/auth/Recovery';
 import RecoveryMessage from 'components/auth/RecoveryMessage';
-import RecoveryForm from 'components/auth/RecoveryForm'
+import RecoveryForm from 'components/auth/RecoveryForm';
 import NewProject from 'components/project_admin/registry/Panel';
 
-<<<<<<< Updated upstream
-class App extends React.Component
-{
-  render()
-  {
-    return (
-      <Switch>
-        <Route path='/login/:action?' component={Login} />
-        <Route path='/logout' component={Logout} />
-        <Route path="/projects/new" component={NewProject} />
-        <Route path='/signup/welcome' component={SignupWelcome} />
-        <Route path='/signup' component={Signup} />
-        <Route path='/recovery_password/:token' component={RecoveryForm} />
-        <Route path='/recovery_message/:type' component={RecoveryMessage} />
-        <Route path='/recovery' component={Recovery} />
-        <Route path='/app' component={Home} />
-        <Redirect to='/app' />
-      </Switch>
-    );
-  }
-=======
 function App() {
   return (
     <Routes>
@@ -52,7 +26,6 @@ function App() {
       <Route path="*" element={<Navigate to="/app" replace />} />
     </Routes>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
