a4931695dd0004b24873e226aac02204ebcb848f
Jose Lorenzo L. Victorino <josevictorino003@gmail>
2025-08-19 20:43:32 +0800

    search changes

--- src/components/search/Hypothesis.js
+++ src/components/search/Hypothesis.js
@@ -1,73 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import redux from 'seed/redux'
 import * as Util from 'seed/util'
-import c from 'resources/css/search/Search.module.css';
 import cx from 'classnames';
 import { Link } from 'react-router-dom'
 import { lcs, lc } from 'components/util/Locales';
 
-class Projects extends React.Component
-{
-  render()
-  {
-    const hypotheses = this.state.hypotheses ? this.state.hypotheses : [];
-    const canvases = this.state.canvases;
-    let projectId = localStorage.getItem('projectId');
-    const project = Util.get(this.props.projects, projectId);
+import c from 'resources/css/search/Search.module.css';
 
-    return (
-      <div className={c.projects}>
-        <h3 className={c.cardsTitle}>{project.name} / {lcs("hypotheses")}</h3>
-        {hypotheses.map(hypothesis =>
-        {
-          let canvas = canvases.filter(canvas => hypothesis.canvas_id == canvas.id)[0];
-          let area = canvas ? canvas.type.areas.filter(a => hypothesis.area_id == a.id)[0] : null;
+function Projects(props) {
+  const { projects, match, getHypothesisList, getCanvasList, getProjectDetailList } = props;
+
+  const [hypotheses, setHypotheses] = useState(null);
+  const [canvases, setCanvases] = useState([]);
+
+  const projectId = localStorage.getItem('projectId');
+  const project = Util.get(projects, projectId);
+  const safeHypotheses = hypotheses || [];
+  
+  useEffect(() => {
+    getHypothesisList({
+      "text.icontains": match.params.search,
+      "canvas.project_id": projectId,
+      "is_active": true
+    }, (res) => setHypotheses(res.body));
+
+    getCanvasList({ "project_id": projectId }, (res) => setCanvases(res.body));
+
+    getProjectDetailList({ project: projectId });
+  }, [match.params.search, getHypothesisList, getCanvasList, getProjectDetailList]);
+
+  return (
+    <>
+      <h4>{`${project.name} / ${lcs("hypotheses")}`}</h4>
+      <div className={c.resultsList}>
+        {safeHypotheses.map((hypothesis) => {
+          const canvas = canvases.find((canvas) => hypothesis.canvas_id === canvas.id);
+          const area = canvas?.type?.areas.find((a) => hypothesis.area_id === a.id);
+
           return (
-            <div className={c.searchCard}>
-              <Link to={`/app/c/${hypothesis.canvas_id}/area/${hypothesis.area_id}/help`} style={{ textDecoration: "none" }}>
-                <div className={c.searchCardInfo}>
-                  <h3 className={c.cardTitle}>{hypothesis.text}</h3>
+            <div key={hypothesis.id} className={c.searchCard}>
+              <Link to={`/app/c/${hypothesis.canvas_id}/area/${hypothesis.area_id}/help`} >
+                <h3 className={c.cardTitle}>{hypothesis.text}</h3>
+                {hypothesis.description &&
                   <p>{hypothesis.description}</p>
-                  <div className={c.compoundSubtitle}>
-                    <div className={c.ring}></div>
-                    <h4 className={c.subtitle}>{canvas ? lc(canvas.type.l_name) : ""} > {area ? lc(area.l_name) : ""}</h4>
-                  </div>
+                }
+                <div className={c.compoundSubtitle}>
+                  <div className={c.ring} />
+                  <p className={c.subtitle}>{`${canvas ? lc(canvas.type.l_name) : ""} > ${area ? lc(area.l_name) : ""}`}</p>
                 </div>
               </Link>
             </div>
           )
         })}
-        {this.state.hypotheses && hypotheses.length == 0 ?
-          <div>{lcs("no_results")}</div> : null}
+        {(hypotheses && safeHypotheses.length === 0) &&
+          <div>{lcs("no_results")}</div>
+        }
       </div>
-    )
-  }
-
-  constructor(props)
-  {
-    super(props);
-    this.state = { hypotheses: null, canvases: [] }
-  }
-
-  componentDidMount()
-  {
-    const { search } = this.props.match.params;
-    let projectId = localStorage.getItem('projectId');
-    const callback = res =>
-      this.setState(s => ({ hypotheses: res.body }));
-    this.props.getHypothesisList({
-      "text.icontains": search,
-      "canvas.project_id": projectId,
-      "is_active": true
-    }, callback);
-
-    const callback1 = res =>
-      this.setState(s => ({ canvases: res.body }));
-    this.props.getCanvasList({
-      "project_id": projectId
-    }, callback1);
-    this.props.getProjectDetailList({ project: projectId });
-  }
+    </>
+  )
 }
 
 export default redux(Projects);
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,69 +1,45 @@
-/*
-__Seed builder__v1.0
-*/
 import * as React from 'react'
-import c from 'resources/css/search/Search.module.css';
-import cx from 'classnames';
 import { Switch, Route, NavLink, withRouter, Redirect } from 'react-router-dom'
-//Components
+import cx from 'classnames';
+
 import Hypothesis from './Hypothesis';
 import Interviews from './Interviews';
 import Projects from './Projects';
 import Users from './Users';
 import { lcs } from 'components/util/Locales';
 
+import c from 'resources/css/search/Search.module.css';
 
-class SearchComponent extends React.Component
-{
-  render()
-  {
-    const { path, url } = this.props.match;
-    const { search } = this.props.match.params;
+function SearchComponent(props) {
+  const { path, url, params: { search } } = props.match;
 
-    return (
-      <section className={c.module}>
-
-        <div className={c.searchBody}>
-          <div>
-            <p className={c.resultsTitle}>{lcs("showing_results_for")} "{search}"</p>
-          </div>
-
-          <div className={c.searchBodyInner}>
-            <div className={c.searchBodyFilters}>
-              <p className={c.filtersTitle}>{lcs("filter")}</p>
-              <NavLink activeClassName={c.active} className={c.btn} exact to={`${url}/hypothesis`}>{lcs("hypotheses")}</NavLink>
-              <NavLink activeClassName={c.active} className={c.btn} to={`${url}/interviews`}>{lcs("interviews")}</NavLink>
-              <NavLink activeClassName={c.active} className={c.btn} to={`${url}/projects`}>{lcs("projects")}</NavLink>
-              <NavLink activeClassName={c.active} className={c.btn} to={`${url}/users`}>{lcs("users")}</NavLink>
-            </div>
-
-            <div className={c.searchBodyResults}>
-
-              <Switch>
-                <Route path={`${path}/hypothesis`} component={Hypothesis} />
-                <Route path={`${path}/interviews`} component={Interviews} />
-                <Route path={`${path}/projects`} component={Projects} />
-                <Route path={`${path}/users`} component={Users} />
-                <Redirect to={`${path}/hypothesis`} />
-              </Switch>
+  return (
+    <section className={cx(c.module, 'container')}>
+      <div style={{paddingBlock: '15px'}}>
+        <h4 className={c.resultsTitle}>{`${lcs("showing_results_for")} "${search}"`}</h4>
+        <div className={cx('row')}>
+          <div className={cx('col-md-4')}>
+            <h4>{lcs("filter")}</h4>
+            <div className={c.filters}>
+              <NavLink activeClassName={c.active} exact to={`${url}/hypothesis`}>{lcs("hypotheses")}</NavLink>
+              <NavLink activeClassName={c.active} to={`${url}/interviews`}>{lcs("interviews")}</NavLink>
+              <NavLink activeClassName={c.active} to={`${url}/projects`}>{lcs("projects")}</NavLink>
+              <NavLink activeClassName={c.active} to={`${url}/users`}>{lcs("users")}</NavLink>
             </div>
           </div>
-
+          <div className={cx(c.searchBodyResults, 'col-md-8')}>
+            <Switch>
+              <Route path={`${path}/hypothesis`} component={Hypothesis} />
+              <Route path={`${path}/interviews`} component={Interviews} />
+              <Route path={`${path}/projects`} component={Projects} />
+              <Route path={`${path}/users`} component={Users} />
+              <Redirect to={`${path}/hypothesis`} />
+            </Switch>
+          </div>
         </div>
-
-      </section>
-    );
-  }
-
-  /*
-  * Business logic
-  */
-
-  constructor(props)
-  {
-    super(props);
-    this.state = {};
-  }
+      </div>
+    </section>
+  );
 }
 
 export default withRouter(SearchComponent);
